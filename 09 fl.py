0e0727a2b7ae8b403633490c9df365baf5f3c97c
naamalevi1357 <naamalevi1357@gmail.com>
2024-09-18 11:54:37 +0300

    Add files via upload

---
+++ 09 fl.py
@@ -0,0 +1,88 @@
+
+# MAKE ALL OF THIS CONDITION FALSE
+
+a = 1
+b = 0
+if a == b:
+    print(f"was True for {a} {b}")
+else:
+    print(f"was False for {a} {b}")
+
+a = 6
+b = 5
+c = 8
+d = 7
+if a + b and c + d:
+    print(f"was True for {a} {b} {c} {d}")
+else:
+    print(f"was False for {a} {b} {c} {d}")
+
+a = 10
+b = 5
+c = 5
+d = 8
+if a >= b or c > d:
+    print(f"was True for {a} {b} {c} {d}")
+else:
+    print(f"was False for {a} {b} {c} {d}")
+
+a = 6
+b = 8
+c = 9
+d = 3
+if a >= b or c < d:
+    print(f"was True for {a} {b} {c} {d}")
+else:
+    print(f"was False for {a} {b} {c} {d}")
+
+a = 6
+b = 8
+c = 2
+d = 1
+if a == b and c <= d:
+    print(f"was True for {a} {b} {c} {d}")
+else:
+    print(f"was False for {a} {b} {c} {d}")
+
+a = 0
+b = 0
+c = 0
+d = 0
+if True and a + b + c + d:
+    print(f"was True for {a} {b} {c} {d}")
+else:
+    print(f"was False for {a} {b} {c} {d}")
+
+
+a = 6
+b = 6
+if a != b:
+    print(f"was True for {a} {b}")
+else:
+    print(f"was False for {a} {b}")
+
+a = 8
+b = 8
+c = 10
+if a != b and a <= c or a <= b or True:
+    print(f"was True for {a} {b} {c}")
+else:
+    print(f"was False for {a} {b} {c}")
+# the result will be true
+
+a = 1
+b = 0
+c = 0
+if a != b and a <= c or a <= b or False:
+    print(f"was True for {a} {b} {c}")
+else:
+    print(f"was False for {a} {b} {c}")
+
+a =5
+b =8
+c = 10
+d = 3
+if a % b == 0 and c % d == 1:
+    print(f"was True for {a} {b} {c} {d}")
+else:
+    print(f"was False for {a} {b} {c} {d}")
